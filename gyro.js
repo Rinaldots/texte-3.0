b93cba2bedcc8074e9151d35ed153caf2589a8d2
Rinaldo <rinaldotavares12@gmail.com>
2024-04-25 23:25:51 -0300

    ok

--- gyro.js
+++ gyro.js
@@ -43,28 +43,7 @@ function onClick() {
   }
   
 }
-if ( location.protocol != "https:" ) {
-  location.href = "https:" + window.location.href.substring( window.location.protocol.length );
-  }
-  function permission () {
-      if ( typeof( DeviceMotionEvent ) !== "undefined" && typeof( DeviceMotionEvent.requestPermission ) === "function" ) {
-          // (optional) Do something before API request prompt.
-          DeviceMotionEvent.requestPermission()
-              .then( response => {
-              // (optional) Do something after API prompt dismissed.
-              if ( response == "granted" ) {
-                  window.addEventListener( "devicemotion", (e) => {
-                      // do something for 'e' here.
-                  })
-              }
-          })
-              .catch( console.error )
-      } else {
-          alert( "DeviceMotionEvent is not defined" );
-      }
-  }
-  const btn = document.getElementById( "request" );
-  btn.addEventListener( "click", permission );
+
 //obtem dados do giroscopio e salva no objeto
 function gyro(event) {
   gyroData.alpha = event.alpha;
