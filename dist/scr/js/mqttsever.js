053db9d8daa333bcd3f80f906911f9df78e1f63f
Rinadots <rinaldotavares12@gmail.com>
2024-04-30 19:30:58 -0300

    DkOS1hkm

---
+++ dist/scr/js/mqttsever.js
@@ -0,0 +1,30 @@
+const options = {
+  // Clean session
+  clean: true,
+  connectTimeout: 4000,
+  // Authentication
+  clientId: 'emqx_'+Math.random().toString(16).substring(2, 8),
+  username: 'example-username',
+  password: '123',
+
+}
+const client = mqtt.connect('ws://192.168.1.100:8080/mqtt',options);
+   
+    client.on('connect', () => {
+      console.log('Connected')
+      client.publish('test', 'Conectado ao broker!');
+    })
+    client.on('error', (err) => {
+      console.log('Connection error: ', err)
+      client.end()
+    })
+    client.on('reconnect', () => {
+      console.log('Reconnecting...')
+    });
+
+  
+    function sendData(){
+      client.publish('test', gyroData.alpha.toFixed(3));
+    }
+
+    setInterval(sendData, 10000);
---
+++ scr/js/animation.js
@@ -0,0 +1,52 @@
+import * as THREE from 'three'
+import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+
+
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+document.body.appendChild( renderer.domElement );
+
+
+const cubeLoader = new THREE.TextureLoader();
+const geometry = new THREE.BoxGeometry( 4, 4, 4 );
+const textureCube = cubeLoader.load("./scr/models/side cube.jpg")
+textureCube.colorSpace = THREE.SRGBColorSpace;
+const material = new THREE.MeshBasicMaterial({
+  map: textureCube,
+});
+const cube = new THREE.Mesh( geometry, material );
+
+
+const planeGeom = new THREE.PlaneGeometry( 50, 50 );
+const back = new THREE.Mesh( planeGeom, new THREE.MeshBasicMaterial( {color: 0xc0c0c0,} ));
+
+
+
+
+scene.add( cube );
+scene.add(back);
+
+camera.position.z = 10;
+back.position.z = -10
+
+function animate() {
+	requestAnimationFrame( animate );
+  
+  cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+  
+  renderer.render( scene, camera );
+ 
+}
+
+animate();
+
+
+
